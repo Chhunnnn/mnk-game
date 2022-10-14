39652bde7fe5e1546dfeb803942f3d458c516907
Chhunnnnn <kimchhuntan1@gmail.com>
2022-10-14 12:42:19 +0900

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+const firebaseConfig = {
+apiKey: process.env.VUE_APP_API_KEY,
+authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+projectId: process.env.VUE_APP_PROJECT_ID,
+storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+appId: process.env.VUE_APP_APP_ID,
+measurementId: process.env.VUE_APP_MEASUREMENT_ID
+};
+initializeApp(firebaseConfig);
+
+createApp(App).use(store).use(router).mount('#app')
